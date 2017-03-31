0d6cc56c389945864ba206223b527bf38aadfb1a
Samantha Geitz <samanthamichele7@gmail.com>
2017-03-31 17:18:56 -0500

    Configure for React

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,13 +8,9 @@
 require('./bootstrap');
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
+ * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+require('./components/Example');
